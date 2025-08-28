9833700eccf985c972861a932001c310b1b5ad19
TRON17 <hkhare360@gmail.com>
2025-08-28 16:07:16 +0530

    Update HelloController.java

--- HelloController.java
+++ HelloController.java
@@ -5,4 +5,10 @@ public class HelloController{
     public String hello(){
     return "Hello World!"
     }
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name){
+    return "Hello" + name +"!";
+     //in Post Mapping you can send some data along with the request.   
+    }
+
 }
