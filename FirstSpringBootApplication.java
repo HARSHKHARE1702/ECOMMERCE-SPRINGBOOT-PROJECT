5289399978a1ae73aa9a3d540ede1479b2764837
TRON17 <hkhare360@gmail.com>
2025-08-28 14:13:52 +0530

    Create FirstSpringBootApplication.java

---
+++ FirstSpringBootApplication.java
@@ -0,0 +1,9 @@
+@SpringbootApplication
+public class FirstSpringApplication{
+  public static Void main(String[] args){
+    SpringApplication.run(FirstSpringApplication.class,args);
+  }
+}
+  
+                          
+                  
