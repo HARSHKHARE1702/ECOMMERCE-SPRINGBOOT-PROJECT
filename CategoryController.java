f6ba688f1b63d2c415796fe7042657706db77ea9
TRON17 <hkhare360@gmail.com>
2025-08-29 13:18:24 +0530

    Update CategoryController.java

--- CategoryController.java
+++ CategoryController.java
@@ -1,3 +1,9 @@
+@RestController
 public class CategoryController{
-
+ 
+  private List<Category> categories = new ArrayList<>();
+  @GetMapping("/api/public/categories")
+  public List<Category> getAllCategories{
+  return categories;
+}
 }
