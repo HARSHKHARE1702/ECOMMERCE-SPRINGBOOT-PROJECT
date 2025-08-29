175770636bbc306817c9d97eccb753293c908724
TRON17 <hkhare360@gmail.com>
2025-08-29 13:21:10 +0530

    Update Category.java

--- Category.java
+++ Category.java
@@ -2,5 +2,8 @@ public class Category{
 
   private Long CategoryId;
   private String CategoryName;
-  
+
+  public Category(long categoryId, String CategoryName){
+          this.categoryId = categoryId;
+          this.categoryName = categoryName;
 }
