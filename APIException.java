59bed81fb4f81aebfac1c6f1432e12ad939addb8
TRON17 <hkhare360@gmail.com>
2025-09-11 16:39:16 +0530

    Create APIException.java

---
+++ APIException.java
@@ -0,0 +1,9 @@
+public class APIException extends RunTimeException {
+private Static final long SerialVersionUID=1L;
+  public APIException(){
+  
+  }
+  public APIException(String Message){
+    super(Message);
+  }
+}
